0f77f263f8861b7d1a1812961e3d0b807b7b3b7e
Eetu Lassi <elassi21@student.oulu.fi>
2023-09-26 18:14:56 +0300

    Added timing attack protection

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -8,6 +8,7 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+    s = s.zfill(1000)
     for c in s:
         if c in ["ä", "ö", "å", "+"]:
             raise ValueError
