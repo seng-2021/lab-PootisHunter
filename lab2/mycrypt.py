eb4d23ec0a1e2bbe95fc81bf8d5e5814a555c2fb
Eetu Lassi <elassi21@student.oulu.fi>
2023-09-26 18:25:00 +0300

    timing attack fix4

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -8,7 +8,7 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
-    s = s.ljust(1000, "y")
+    s = s.ljust(1000, "a")
     for c in s:
         if c in ["ä", "ö", "å", "+"]:
             raise ValueError
